dce28506c3d5b711c97063ad96c09eca00a1ae67
Peter Zhang <peter.zhang.henan@gmail.com>
2022-03-08 17:51:49 +0800

    增加less解析

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,16 @@
 <template>
-  <h1>Hello World</h1>
+  <div>
+    <img src="/nuxt.svg" alt="logo" width="100" />
+    <h1>Hello World</h1>
+    <img src="@/assets/dog.png" width="200" />
+    <div class="box"></div>
+  </div>
 </template>
+<style lang="less">
+  .box {
+    width: 200px;
+    height: 200px;
+    background: red url('~/assets/dog.svg') no-repeat center;
+    background-size: 50% 50%;
+  }
+</style>
